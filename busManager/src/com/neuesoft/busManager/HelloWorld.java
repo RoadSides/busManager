dea685084c47019c4c7487f1be76e6d83ea2a00a
RoadSides <1813568679@qq.com>
2017-09-07 11:26:10 +0800

    shiyixia

--- busManager/src/com/neuesoft/busManager/HelloWorld.java
+++ busManager/src/com/neuesoft/busManager/HelloWorld.java
@@ -3,5 +3,7 @@ package com.neuesoft.busManager;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		System.out.println("haha");
+		System.out.println("haha1");
 	}
 }
